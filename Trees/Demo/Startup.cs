acdc8df79cc194a62250d0024ee522f04d527d34
l3kov9 <l3kov.9@gmail.com>
2017-10-13 13:02:58 +0300

    Update trees

---
+++ Trees/Demo/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var tree = new Tree<int>(4, new Tree<int>(9, new Tree<int>(6), new Tree<int>(5)), new Tree<int>(14)
+                , new Tree<int>(15, new Tree<int>(8)));
+            tree.Print();
+
+            IncreaseWithOne(tree);
+
+            tree.Print();
+        }
+
+        private static void IncreaseWithOne(Tree<int> tree)
+        {
+            tree.Value++;
+            foreach (var child in tree.Children)
+            {
+                IncreaseWithOne(child);
+            }
+        }
+
+        private static List<int> GetElements(Tree<int> tree)
+        {
+            var list = new List<int>();
+            list.Add(tree.Value);
+
+            foreach (var child in tree.Children)
+            {
+            }
+
+            return list;
+        }
+    }
+}
---
+++ Trees/Trees/Tree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Tree<T>
+{
+    public T Value { get; set; }
+
+    public List<Tree<T>> Children;
+
+    public Tree(T value, params Tree<T>[] children)
+    {
+        this.Value = value;
+        this.Children = new List<Tree<T>>();
+
+        foreach (var child in children)
+        {
+            this.Children.Add(child);
+        }
+    }
+
+    public void Print(int indent = 0)
+    {
+        Console.WriteLine(new string(' ',indent*2)+ this.Value);
+
+        foreach (var child in this.Children)
+        {
+            child.Print(indent + 1);
+        }
+    }
+
+    public void Each(Action<T> action)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> OrderDFS()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> OrderBFS()
+    {
+        throw new NotImplementedException();
+    }
+}
